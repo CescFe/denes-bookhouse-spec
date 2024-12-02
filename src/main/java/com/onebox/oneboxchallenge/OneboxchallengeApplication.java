5eecd4d62fe3d6132b63cc9def3feb6df9e84ad5
Cesc Fe <76252400+FrancescFe@users.noreply.github.com>
2024-12-02 11:13:07 +0100

    init commit

---
+++ src/main/java/com/onebox/oneboxchallenge/OneboxchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.onebox.oneboxchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneboxchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneboxchallengeApplication.class, args);
+	}
+
+}
