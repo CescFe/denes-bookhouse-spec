9abbe60b930cf3b90671114419be14270088c73d
Cesc Fe <76252400+FrancescFe@users.noreply.github.com>
2024-12-03 15:31:19 +0100

    Feat (OB-5): add scheduled delete

--- src/main/java/com/onebox/oneboxchallenge/cart/infrastructure/repository/SpringDataCartRepository.java
+++ src/main/java/com/onebox/oneboxchallenge/cart/infrastructure/repository/SpringDataCartRepository.java
@@ -3,5 +3,8 @@ package com.onebox.oneboxchallenge.cart.infrastructure.repository;
 import com.onebox.oneboxchallenge.cart.infrastructure.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface SpringDataCartRepository extends JpaRepository<CartEntity, Long> {
+    void deleteByUpdatedAtBefore(LocalDateTime timestamp);
 }
